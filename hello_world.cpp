d10f9a73fcc03a358d9a3b64ded218d344e38fd6
den4ick800 <danilka_pr@mail.ru>
2021-03-17 00:49:15 -0700

    real new comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -13,9 +13,12 @@ main()
 =======
 int main()
 {
+//create for getting name
 String name_1;
+//ask for name
 std::cout<<"Write your name:";
 std::cin>>name_1;
+//write answer
 std::cout<<"Hello, world!!!"<<name<<std::endl;
 return 0;
 >>>>>>> hello_world delete std
