8c93974dd76c624bc42020c23b63f9e8100f322a
den4ick800 <danilka_pr@mail.ru>
2021-03-30 11:30:15 -0700

    new stil'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main()
+int
+main()
 {
-String name;
-cout<<"Write your name:";
-cin>>name;
-cout<<"Hello, world!!!"<<name<<endl;
-return 0;
+  String name;
+  cout << "Write your name:";
+  cin >> name;
+  cout << "Hello, world!!!" << name << endl;
+  return 0;
 }
-
